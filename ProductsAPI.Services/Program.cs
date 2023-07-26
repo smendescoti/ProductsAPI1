f22d1e5526e6e686c0fa16f23b02f0e2f81b6b2c
smendescoti <sergio.coti@gmail.com>
2023-07-26 16:36:50 -0300

    first commit

---
+++ ProductsAPI.Services/Program.cs
@@ -0,0 +1,30 @@
+using MediatR;
+using ProductsAPI.Application.Interfaces.Services;
+using ProductsAPI.Application.Services;
+using ProductsAPI.Infra.IoC.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSqlServerConfig(builder.Configuration); //SqlServer
+builder.Services.AddMongoDBConfig(builder.Configuration); //MongoDB
+builder.Services.AddDependencyInjection(); //Serviços
+builder.Services.AddMediatRConfig(); //MediatR
+builder.Services.AddJwtBearer(builder.Configuration); //JWT   
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+public partial class Program { }
---
+++ ProductsAPI.Tests/Helpers/TestHelper.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductsAPI.Tests.Helpers
+{
+    public static class TestHelper
+    {
+        /// <summary>
+        /// Método para criar um client http da api de produtos
+        /// </summary>
+        public static HttpClient CreateClient
+        {
+            get
+            {
+                var accessToken = @"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoie1wiSWRcIjpcIjg1ODc5MjE4LWQ5N2YtNDgwYy04YzA4LTJjOTc2YzMyZDZmNlwiLFwiTmFtZVwiOlwiU2VyZ2lvIE1lbmRlc1wiLFwiRW1haWxcIjpcInNlcmdpby5jb3RpQHlhaG9vLmNvbVwiLFwiUm9sZVwiOlwiVVNFUl9ST0xFXCIsXCJTaWduZWRBdFwiOlwiMjAyMy0wNy0yNFQyMTo0NTozNC41MTEyMDI0LTAzOjAwXCJ9IiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiVVNFUl9ST0xFIiwiZXhwIjoxNjkwMjQ2ODM0LCJpc3MiOiJ1c2Vyc2FwcCIsImF1ZCI6IioifQ.aF7Niuycqh2W0T208kLSl5ktaQ7mZVIy6B2WljBfqrk";
+                var auth = new AuthenticationHeaderValue("Bearer", accessToken);
+
+                var client = new WebApplicationFactory<Program>().CreateClient();
+                client.DefaultRequestHeaders.Authorization = auth;
+
+                return client;
+            }
+        }
+
+        /// <summary>
+        /// Método para serializar o conteudo da requisição que será enviada para um serviço
+        /// </summary>
+        public static StringContent CreateContent<TRequest>(TRequest request)
+            => new StringContent(JsonConvert.SerializeObject(request),
+                Encoding.UTF8, "application/json");
+
+        /// <summary>
+        /// Método para deserializar o retorno obtido pela execução de um serviço
+        /// </summary>
+        public static TResponse ReadResponse<TResponse>(HttpResponseMessage message)
+            => JsonConvert.DeserializeObject<TResponse>(message.Content.ReadAsStringAsync().Result);
+    }
+}
